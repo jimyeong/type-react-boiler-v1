5f4e938fdbb211b2d54d897077ada2148a6b5e17
jimyeong <idjjm92@gmail.com>
2019-02-13 16:33:23 +0900

    added optional chaining

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { Router } from 'react-router';
+import {createBrowserHistory} from 'history';
+
+const history = createBrowserHistory();
+
+class App extends React.Component<any> {
+
+    componentDidMount() {
+
+    }
+
+
+    public render() {
+
+        return(
+            <Router history={ history }>
+                <React.Fragment>
+                    hello world
+                    
+                </React.Fragment>
+            </Router>
+        )
+    }
+}
+
+export default App;
