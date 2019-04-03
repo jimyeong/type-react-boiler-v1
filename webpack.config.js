23d9aff7231d0ed13f6d8605d437f028b5956228
jimyeong <idjjm92@gmail.com>
2019-04-03 10:50:36 +0900

    loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,78 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: "./src/index.tsx",
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: "bundle.js"
-    },
-    resolve: {
-        extensions:['.tsx','.ts','.js','.json']
-    },
-    module: {
-        rules:[
-            {
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            '@babel/preset-env',
-                            "@babel/plugin-proposal-optional-chaining"
-                        ]
-                    }
-                }
-            },
-            {
-                test: /\.less$/,
-                use:[
-                    {
-                        loader: MiniCssExtractPlugin.loader
-                    },
-                    "css-loader",
-                    "less-loader"
-                ]
-            },
-            {
-                test: /\.tsx?$/,
-                loader: "awesome-typescript-loader"
-            }
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".json"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/plugin-proposal-optional-chaining"]
+          }
+        }
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          "css-loader",
+          "less-loader"
         ]
-    },
-    devServer: {
-        historyApiFallback: true
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename:"[name].css",
-            chunkFilename:"[id].css"
-        })
+      },
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader"
+      },
+
+      {
+        test: /\.(jpg|png|gif|svg|pdf|ico)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[path][name]-[hash:8].[ext]"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|svg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 100000
+            }
+          }
+        ]
+      }
     ]
-}; 
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
+};
